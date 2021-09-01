baea16f245c51641d852e4e6bac86bf10c3169a7
Pranav Singhal <57180742+pranavdex@users.noreply.github.com>
2021-09-01 23:27:10 +0530

    Day16

---
+++ Hackerrank-Python/Day_16_mutations.py
@@ -0,0 +1,9 @@
+def mutate_string(string, position, character):
+    pos = position+1
+    Output = string[:position]+character+string[pos:]
+    return Output
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
